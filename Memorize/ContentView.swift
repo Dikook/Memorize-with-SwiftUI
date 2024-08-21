3ff1a6e922eb0759ae6fe7e1996c66890e7fa326
Dikook <vimvom931@gmail.com>
2024-08-21 14:57:00 +0500

    adding a add button. Is work very cool.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["😀", "😃", "😄", "😁", "😇", "3", "4", "5", "6", "7"]
+    @State var countCard: Int = 4
+    
+    
     var body: some View {
         HStack {
-            RoundedNumberOne()
-            RoundedNumberOne(faceIsUp: true)
-            RoundedNumberOne()
-            RoundedNumberOne()
-            
+            ForEach(0..<countCard, id: \.self) { index in
+                RoundedNumberOne(emoji: emojis[index])
+            }
+            Button("Add Card") {
+                countCard = countCard + 1
+            }
         }
         .foregroundColor(.orange)
         .padding()
@@ -26,19 +31,23 @@ struct ContentView: View {
 }
 
 struct RoundedNumberOne: View {
-    var faceIsUp: Bool = false
+    var emoji: String
+    @State var faceIsUp: Bool = true
     
     var body: some View {
-        ZStack( content: {
+        ZStack {
             if faceIsUp {
                 RoundedRectangle(cornerRadius: 12)
                     .foregroundColor(.white)
                 RoundedRectangle(cornerRadius: 12)
                     .strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(emoji).font(.largeTitle)
             } else {
                 RoundedRectangle(cornerRadius: 12)
             }
-        })
+        }
+        .onTapGesture {
+            faceIsUp.toggle()
+        }
     }
 }
